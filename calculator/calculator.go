7ef0e7dba57d441d6f5a48d2c652b8174926fedd
Konstantin Afonin <konstantin.afonin@gmail.com>
2019-01-28 08:42:08 +0100

    initial commit of 'calculator' package

---
+++ calculator/calculator.go
@@ -0,0 +1,33 @@
+package calculator
+
+import (
+	"errors"
+	"fmt"
+	"math/big"
+)
+
+/* TODO -- clarify constants in Go */
+/* TODO -- consider enum */
+const (
+	ADD       = "ADD"
+	DIVIDE    = "DIVIDE"
+	SUBSTRACT = "SUBSTRACT"
+	MULTIPLY  = "MULTIPLY"
+)
+
+func Calculate(operation string, operandA *big.Float, operandB *big.Float) (result *big.Float, err error) {
+	switch operation {
+	case ADD:
+		result, err = new(big.Float).Add(operandA, operandB), nil
+	case SUBSTRACT:
+		result, err = new(big.Float).Sub(operandA, operandB), nil
+	case MULTIPLY:
+		result, err = new(big.Float).Mul(operandA, operandB), nil
+	case DIVIDE:
+		result, err = new(big.Float).Quo(operandA, operandB), nil
+	default:
+		result, err = nil, errors.New(fmt.Sprintf("Invalid operation %v", operation))
+	}
+
+	return
+}
---
+++ calculator/calculator_test.go
@@ -0,0 +1,6 @@
+package calculator
+
+/* TODO -- test for ADD -Inf +Inf */
+/* TODO -- test for SUBSTRACT infinities with equal signs */
+/* TODO -- test for MULTIPLY 0 and +-Inf */
+/* TODO -- test for DIVIDE both infinities */
