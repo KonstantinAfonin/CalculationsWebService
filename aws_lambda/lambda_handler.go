ceae7e5ab924df238595d32796e121d7ba96681f
Konstantin Afonin <konstantin.afonin@gmail.com>
2019-02-01 00:46:12 +0100

    added logs to lambda handler

--- aws_lambda/lambda_handler.go
+++ aws_lambda/lambda_handler.go
@@ -4,10 +4,14 @@ import (
 	"context"
 	"github.com/KonstantinAfonin/CalculatorLambdaHandler/json"
 	"github.com/aws/aws-lambda-go/lambda"
+	"log"
 )
 
 func HandleRequest(ctx context.Context, calculateRequest json.CalculateRequest) (*json.CalculateResponse, error) {
-	return json.Calculate(&calculateRequest)
+	log.Printf("request: %v", calculateRequest)
+	response, err := json.Calculate(&calculateRequest)
+	log.Printf("response: %v, error: %v", *response, err)
+	return response, err
 }
 
 func main() {
